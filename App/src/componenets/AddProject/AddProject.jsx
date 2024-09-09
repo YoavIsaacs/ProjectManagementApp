c780f97ddceac4b8061f8d1f34cbae82d8dd7df7
Yoav Isaacs <yoav.isaacs@gmail.com>
2024-09-09 23:26:02 +0300

    [AddProject] Added rest of the fields for the input

--- App/src/componenets/AddProject/AddProject.jsx
+++ App/src/componenets/AddProject/AddProject.jsx
@@ -5,6 +5,10 @@ export default function AddProject() {
         <div>
             <label>Title</label>
             <input type="text"></input>
+            <label>Description</label>
+            <input type="text"></input>
+            <label>Due Date</label>
+            <input type="date"></input>
         </div>
     );
 }
