29dbe221244eaeb2a37b3a198ebfb2fd1ea8bd26
Yoav Isaacs <yoav.isaacs@gmail.com>
2024-09-12 23:40:49 +0300

    [main] Added conditional showing add new project / fallback

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
+
 import NewProject from "./componenets/NewProject/NewProject";
 import NoProjectSelected from "./componenets/NoProjectSelected/NoProjectSelected";
 import Sidebar from "./componenets/Sidebar/Sidebar";
 
 function App() {
 
+  const [projectState, setProjectState] = useState({
+    selectedProjectID: undefined,
+    projects: []
+  });
+
+
+  function handleStartAddProject() {
+    setProjectState(prev => {
+      return {
+        ...prev,
+        selectedProjectID: null
+      };
+    });
+  }
+  let content;
+
+if (projectState.selectedProjectID === null) {
+  content = <NewProject />
+} else if (projectState.selectedProjectID === undefined) {
+  content = <NoProjectSelected onStartAddingProject={handleStartAddProject}/>
+}
+
+
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <Sidebar onStartAddingProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
