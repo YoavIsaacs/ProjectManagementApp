e2f03d2527aa58556a51af7eaa2cfcc9d0bad242
Yoav Isaacs <yoav.isaacs@gmail.com>
2024-09-15 22:41:18 +0300

    [Tasks] Added new tasks component

--- App/src/componenets/SelectedProject/SelectedProject.jsx
+++ App/src/componenets/SelectedProject/SelectedProject.jsx
@@ -1,4 +1,5 @@
 import Button from "../Button/Button";
+import Tasks from "../Tasks/Tasks";
 
 
 
@@ -20,6 +21,7 @@ export default function SelectedProject({project, onDelete}) {
                 <p className="mb-4 text-stone-400">{formattedDate}</p>
                 <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
             </header>
+            <Tasks />
         </div>
     );
 }
---
+++ App/src/componenets/Tasks/Tasks.jsx
@@ -0,0 +1,14 @@
+
+export default function Tasks() {
+    return(
+        <section>
+            <h2 className="text-2xl font-bold text-stone-700 mb-4">
+                TASKS
+            </h2>
+            <p className="text-stone-800 mb-4">
+                NO TASKS YET
+            </p>
+            <ul></ul>
+        </section>
+    );
+}
